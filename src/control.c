8fa5c855b97091e0651c5071d025c8a5260a1b8c
agueo <ahlopez@ncsu.edu>
2017-05-15 15:53:09 -0400

    Added a new file that will allow you to control the robot.
    Files added or modified:
        - Control.c
        - Makefile
    
        Control.c adds logic to allow you to control the robot.
        Makefile rule to create the new file.

---
+++ src/control.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wiringPi.h>
+
+#define M1_FWD 23
+#define M1_BWD 24
+#define M2_FWD 17
+#define M2_BWD 27
+
+void forward(int _delay);
+void backwards(int _delay);
+void right_turn(int _delay);
+void left_turn(int _delay);
+
+int main()
+{
+	/* initialize the wiringPi library */
+	unsigned int _delay = 1000;
+	wiringPiSetup();
+	wiringPiSetupGpio();
+	char cmd;
+	// TODO - add running loop
+	while(1)
+	{
+		scanf("%c",&cmd);
+		switch(cmd){
+			case('w'):
+				forward(_delay);
+				break;
+			case('s'):
+				backwards(_delay);
+				break;
+			case('a'):
+				left_turn(_delay);
+				break;
+			case('d'):
+				right_turn(_delay);
+				break;
+			case('q'):
+				return 0;
+			default:
+				printf("Not a valid command");
+				break;
+	}
+
+	return 0;
+}
+
+/* Forward function - Move the car forward
+ * input: delay - How long the car should move forward for
+ * ouput: none
+ */
+void forward(int _delay)
+{
+	digitalWrite(M1_FWD, HIGH);
+	digitalWrite(M2_FWD, HIGH);
+	delay(_delay);
+	digitalWrite(M1_FWD, LOW);
+	digitalWrite(M1_FWD, LOW);
+}
+
+/* Backwards function - Move the car in reverse
+ * input: delay - How long the car should reverse
+ * output: none
+ */
+void backwards(int _delay)
+{
+	digitalWrite(M1_BWD, HIGH);
+	digitalWrite(M2_BWD, HIGH);
+	delay(_delay);
+	digitalWrite(M1_BWD, LOW);
+	digitalWrite(M2_BWD, LOW);
+}
+
+/* right_turn - Turn the car to the right
+ * input: delay - How long the car will turn right
+ * output: none
+ */
+void right_turn(int _delay)
+{
+	digitalWrite(M1_BWD, HIGH);
+	digitalWrite(M2_FWD, HIGH);
+	delay(_delay);
+	digitalWrite(M1_BWD, LOW);
+	digitalWrite(M2_FWD, LOW);
+}
+
+/* left_turn - Turn the car to the left
+ * input: delay - How long the car will turn left
+ * ouput: none
+ */
+void left_turn(int _delay)
+{
+	digitalWrite(M1_FWD, HIGH);
+	digitalWrite(M2_BWD, HIGH);
+	delay(_delay);
+	digitalWrite(M1_FWD, LOW);
+	digitalWrite(M2_BWD, LOW);
+}
+
+
